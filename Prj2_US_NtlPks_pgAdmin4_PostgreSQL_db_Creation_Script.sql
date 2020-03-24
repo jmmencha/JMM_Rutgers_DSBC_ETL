f1ad6057572509f219fdf7c0f636a531bdc91135
Jacob Menchak <55728549+jmmencha@users.noreply.github.com>
2020-03-24 15:36:54 -0400

    Add files via upload

---
+++ Prj2_US_NtlPks_pgAdmin4_PostgreSQL_db_Creation_Script.sql
@@ -0,0 +1,13 @@
+-- Database: Prj2_US_NtlPks_pgAdmin4_PostgreSQL_db
+
+-- DROP DATABASE "Prj2_US_NtlPks_pgAdmin4_PostgreSQL_db";
+
+CREATE DATABASE "Prj2_US_NtlPks_pgAdmin4_PostgreSQL_db"
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'English_United States.1252'
+    LC_CTYPE = 'English_United States.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+	
